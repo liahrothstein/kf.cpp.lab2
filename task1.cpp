ff4c19feaf5627568a550807c485ef2b6071b93c
liahrothstein <liahrothstein@gmail.com>
2026-03-17 12:06:35 +0300

    fix: rebase functions to other files

--- task1.cpp
+++ task1.cpp
@@ -1,31 +1,14 @@
 #include <iostream>
 
+#include "./func/print-info.h"
+#include "./func/calc-pres.h"
+
 using namespace std;
-
-// Функция для вывода информации о программе и авторе
-void printInfo() {
-    cout << "Программа вычисляет силу давления в воде на глубине H метров" << endl;
-    cout << "\nИсполнитель: Новиков Д. А." << endl;
-    cout << "Вариант: №14" << endl;
-}
-
-/*
-Функция вычисления давления
-Принимает глубину h, возвращает давление
-*/
-double calculatePressure(double h) {
-    const double rho = 1000.0; // Плотность воды (кг/м3)
-    const double g = 9.81;     // Ускорение свободного падения (м/с2)
-    
-    double pressure = rho * g * h;
-
-    return pressure;
-}
 
 int main() {
     double H;
 
-    printInfo();
+    printInfo("силу давления в воде на глубине H метров");
     
     // Ввод исходных данных с указанием единиц измерения
     cout << "\nВведите глубину H (в метрах): ";
